a4376822fc98911461979d643f801be0750c1dbf
arshak <arshakr10@gmail.com>
2019-09-26 16:59:33 +0300

    New

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sey somethink");
+            string name = (Console.ReadLine());
+            for (var  i = name.Length -1; i > -1; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }          
+    }
+}
